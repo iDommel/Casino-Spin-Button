26ab3904ebb1a8f870de7d1160acfc682fe657c4
Iona Dommel <iona.dommel-prioux@epitech.eu>
2023-11-14 16:53:50 +0100

    👷 💄 UPDATED: css for tablet, fixed button presses, added build pipeline for apk

--- App.js
+++ App.js
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from 'react';
-import { View, TouchableOpacity, ImageBackground, Text, StyleSheet } from 'react-native';
+import { View, TouchableHighlight, ImageBackground, Text, StyleSheet } from 'react-native';
 
 import { Audio } from 'expo-av';
 import { ConfettiEffect } from './src/ConfettiEffect'; // Import the confetti effect
 
 export default function App() {
-  const [triggerConfetti, setTriggerConfetti] = useState(false);
   const fullsound_file = require('./assets/fullsound.wav');
   const image = require("./assets/background1.png");
+  const [isPressed, setIsPressed] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  useEffect(() => {
-    if (triggerConfetti) {
-      setTriggerConfetti(false); // Reset the confetti effect
+  const handlePressIn = async () => {
+    setIsPressed(true);
+    if (!isPlaying) {
+      setIsPlaying(true);
+      const { sound: loadedfullsound } = await Audio.Sound.createAsync(fullsound_file);
+
+      await loadedfullsound.playAsync();
+      loadedfullsound.setOnPlaybackStatusUpdate(playbackStatus => {
+        if (!playbackStatus.isPlaying) {
+          setIsPlaying(false); // Re-enable the button when audio finishes
+        }
+      });
     }
-  }
-  , [triggerConfetti]);
-
-
-  const handlePress = async () => {
-    const { sound: loadedfullsound } = await Audio.Sound.createAsync(fullsound_file);
-
-    await loadedfullsound.playAsync();
-    setTriggerConfetti(true); // Trigger the confetti effect
   };
 
-  return (
+  const handlePressOut = async () => {
+    setIsPressed(false);
+  };
 
+
+  return (
       <View style={styles.container}>
           <ImageBackground source={image} resizeMode="cover" style={styles.image} >
-            <ConfettiEffect trigger={triggerConfetti} />
-            <TouchableOpacity style={styles.spinButton} onPress={handlePress}>
-              <Text style={styles.spinButtonText}>Spin !</Text>
-            </TouchableOpacity>
-          </ImageBackground>
+            <ConfettiEffect trigger={isPlaying} />
+              <TouchableHighlight
+                disabled={isPlaying} // Disable the button while playing
+                onPressIn={handlePressIn}
+                onPressOut={handlePressOut}
+                underlayColor={''} // Optional: color for the underlay
+                style={[styles.spinButton, (isPressed && styles.pressedButton) || (isPlaying && styles.disabledButton)]}
+              >
+                <Text disabled={isPlaying} style={styles.spinButtonText}>SPIN</Text>
+              </TouchableHighlight>
+            </ImageBackground>
       </View>
   );
 }
@@ -51,14 +62,14 @@ const styles = StyleSheet.create({
     alignItems: "center", // Center content horizontally within the image
   },
   spinButton: {
-    backgroundColor: '#FF0000', // Red background
-    width: 100, // Adjust size as needed
-    height: 100, // Adjust size as needed
+    backgroundColor: '#F2064C', // Red background
+    width: 300, // Adjust size as needed
+    height: 300, // Adjust size as needed
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 50, // Half of width and height to make it circle
+    borderRadius: 150, // Half of width and height to make it circle
     // Adding a white border
-    borderWidth: 2, // Width of the border
+    borderWidth: 4, // Width of the border
     borderColor: '#FFFFFF', // White border
 
     // Adding a shadow
@@ -68,11 +79,22 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84, // Shadow blur radius
 
     // For Android, elevation helps to create a shadow effect
-    elevation: 5,
+    elevation: 10,
+  },
+  disabledButton: {
+    backgroundColor: '#999999', // Greyed out
+    elevation: 0, // No shadow for disabled state
+  },
+
+  pressedButton: {
+    elevation: 2, // Reduced elevation when pressed
+    shadowOffset: { width: 0, height: 5 }, // Adjusted shadow position
+    backgroundColor: '#b00135', // Optional: different background color when pressed
   },
   spinButtonText: {
     color: '#FFFFFF', // White text
     fontWeight: 'bold',
+    fontSize: 40,
   },
 });
 
